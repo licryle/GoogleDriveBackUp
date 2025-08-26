9b34c03dadf2d06be13fa7abce9828dd3ee7bf45
licryle <cyrille@berliat.fr>
2025-08-26 22:55:09 +0800

    removing unused dependencies in gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,19 +42,9 @@ kotlin {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("com.google.android.material:material:1.12.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.3.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.2")
-
 
     // Let's have a gDrive Cloud Backup
     implementation("com.google.android.gms:play-services-auth:21.4.0")
-    implementation("com.google.api-client:google-api-client:2.8.1")
-    implementation("com.google.api-client:google-api-client-android:2.8.1")
     implementation("com.google.apis:google-api-services-drive:v3-rev20240509-2.0.0")
-    implementation("com.google.http-client:google-http-client-gson:2.0.0")
 }
