dd1b9bbbe4af2a19ddb05f25bbfae2cfd0bf1d92
licryle <cyrille@berliat.fr>
2026-02-26 00:53:33 +0800

    Simplify Build file to support cocoapods.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,17 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.multiplatform")
+    id("org.jetbrains.kotlin.native.cocoapods")
 }
 
-android {
+extensions.configure<com.android.build.api.dsl.LibraryExtension> {
     namespace = "fr.berliat.googledrivebackup"
     compileSdk = 36
 
     defaultConfig {
         minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -45,22 +35,39 @@ kotlin {
     iosX64()
     iosArm64()
     iosSimulatorArm64()
+
+    cocoapods {
+        summary = "Google Drive Backup module for HSK Flashcards"
+        homepage = "https://github.com/Licryle/HSKFlashcardsWidget"
+        version = "1.0"
+        ios.deploymentTarget = "16.0"
+        framework {
+            baseName = "googledrivebackup"
+        }
+    }
     
     sourceSets {
-        commonMain.dependencies {
-            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-            implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.7.1-0.6.x-compat")
-            implementation("io.github.vinceglb:filekit-core:0.13.0")
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.7.1-0.6.x-compat")
+                implementation("io.github.vinceglb:filekit-core:0.13.0")
+            }
         }
-        androidMain.dependencies {
-            implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.3") // Downgraded slightly to match stable if needed, sticking to what was there or close suitable for KMP
-            
-            // Let's have a gDrive Cloud Backup
-            implementation("com.google.android.gms:play-services-auth:21.4.0")
-            implementation("com.google.apis:google-api-services-drive:v3-rev20250829-2.0.0")
+        val androidMain by getting {
+            dependencies {
+                implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
+                implementation("com.google.android.gms:play-services-auth:21.5.1")
+                implementation("com.google.apis:google-api-services-drive:v3-rev20250829-2.0.0")
+            }
         }
-        iosMain.dependencies {
-            
+        
+        // Explicitly create iosMain and link it to targets
+        val iosMain by creating {
+            dependsOn(commonMain)
         }
+        val iosX64Main by getting { dependsOn(iosMain) }
+        val iosArm64Main by getting { dependsOn(iosMain) }
+        val iosSimulatorArm64Main by getting { dependsOn(iosMain) }
     }
 }
